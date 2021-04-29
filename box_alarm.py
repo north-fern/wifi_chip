6ae5b0de23e618db731628421ff5d0391d33fa23
north-fern <60278154+north-fern@users.noreply.github.com>
2021-04-29 11:02:16 -0400

    Update box_alarm.py

--- box_alarm.py
+++ box_alarm.py
@@ -4,8 +4,8 @@ serial = hub.port.B
 serial.mode(hub.port.MODE_FULL_DUPLEX)
 serial.baud(115200)
 power = hub.port.B.pwm(100)
-force = hub.port.C.device
-
+force = hub.port.A.device
+force.get()[0]
 
 ssid = 'FiOS-9Y9Z9'
 pswd = 'tux6789duck210gate'
@@ -20,9 +20,23 @@ serial.write('\r\n')
 utime.sleep(1)
 serial.read(1000)
 serial.read(1000)
-def send_messages(command_set):
+def send_messages(command_set,Field,Value):
     ans = []
-    for i in command_set:
+    command_set_put = ['headers = {"Accept"',':"applicaiton/json",','"Content-Type":"appli','cation/json","Auth','orization":"Bearer " + "', API_Key,'"}\r\n', 'urlValue ="', urlBase ,'" + "', BaseID, '" + "/" + "', Table.replace(" ","%20"), '"\r\n', 'propValue={"records"',':[{"fields":{"',Field,'":"',Value,'"} } ]}\r\n','val=urequests.post','(urlValue,headers=headers,','json=propValue).text\r\n','data=ujson.loads(val)\r\n','result=data.get(','"records")[-1]','.get("id")\r\n','print(result)\r\n']
+    command_set_get = ['headers = {"Accept"',':"applicaiton/json",','"Content-Type":"appli','cation/json","Auth','orization":"Bearer " + "', API_Key,'"}\r\n', 'urlValue ="', urlBase ,'"+"', BaseID, '"+ "/" + "', Table.replace(" ","%20"), '"+"?view=Grid%20view"\r\n', 'val = urequests.get(','urlValue,headers=headers)','.text\r\n', 'data = ujson.loads(val)\r\n','ans = data.get("records")','[-2].get("fields")','.get("',Field,'")\r\n','print(ans)\r\n']
+    command_set_setup = ['import network \r\n','import ujson \r\n','import urequests\r\n', 'import os \r\n']
+    command_set_wifi = ['wifi=network.WLAN','(network.STA_IF)\r\n','wifi.active(True)\r\n','wifi.connect("', ssid , '","', pswd, '")\r\n','a=wifi.isconnected()\r\n','print(a)\r\n']
+
+    if command_set == 'get':
+        command_set_sent = command_set_get
+    if command_set == 'put':
+        command_set_sent = command_set_put
+    if command_set == 'wifi':
+        command_set_sent = command_set_wifi
+    if command_set == 'setup':
+        command_set_sent = command_set_setup
+
+    for i in command_set_sent:
         serial.write(i)
         utime.sleep(.25)
         add1 = serial.read(1000)
@@ -38,16 +52,9 @@ def get_time():
 
 
 #based on milan's airtable.py command set
-command_set_setup = ['import network \r\n','import ujson \r\n','import urequests\r\n', 'import os \r\n']
-
-command_set_put = ['headers = {"Accept"',':"applicaiton/json",','"Content-Type":"appli','cation/json","Auth','orization":"Bearer " + "', API_Key,'"}\r\n', 'urlValue ="', urlBase ,'" + "', BaseID, '" + "/" + "', Table.replace(" ","%20"), '"\r\n', 'propValue={"records"',':[{"fields":{"',Field,'":"',Value,'"} } ]}\r\n','val=urequests.post','(urlValue,headers=headers,','json=propValue).text\r\n','data=ujson.loads(val)\r\n','result=data.get(','"records")[-1]','.get("id")\r\n','print(result)\r\n']
-
-command_set_get = ['headers = {"Accept"',':"applicaiton/json",','"Content-Type":"appli','cation/json","Auth','orization":"Bearer " + "', API_Key,'"}\r\n', 'urlValue ="', urlBase ,'"+"', BaseID, '"+ "/" + "', Table.replace(" ","%20"), '"+"?view=Grid%20view"\r\n', 'val = urequests.get(','urlValue,headers=headers)','.text\r\n', 'data = ujson.loads(val)\r\n','ans = data.get("records")','[-2].get("fields")','.get("',Field,'")\r\n','print(ans)\r\n']
-
-command_set_wifi = ['wifi=network.WLAN','(network.STA_IF)\r\n','wifi.active(True)\r\n','wifi.connect("', ssid , '","', pswd, '")\r\n','a=wifi.isconnected()\r\n','print(a)\r\n']
 
 
-ans = send_messages(command_set_setup)
+ans = send_messages('setup','','')
 
 def extract_command(last_command):
     step_zero = last_command.decode()
@@ -58,17 +65,21 @@ def extract_command(last_command):
     return step_four
 
 print(ans)
-ans = send_messages(command_set_wifi)
+ans = send_messages('wifi','','')
 print(ans)
 
 while True:
+    val = force.get()[0]
+    print(val)
+    '''
     if force.is_pressed():
         time_string = get_time()
         Value = time_string.string()
         Field = 'data2'
-        ans2 = send_messages(command_set_put)
+        ans2 = send_messages('put',Field, Value)
         print(ans2)
         serial.write('\r\n')
         trash1 = serial.read(1000)
         serial.write('\r\n')
         trash2 = serial.read(1000)
+    '''
